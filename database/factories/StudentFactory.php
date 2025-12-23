4a13ffacbfb2b96d221fc565aefdff914dfb1873
Hariom Kashyap <omkashyap6870@gmail.com>
2025-12-23 17:22:03 +0530

    seed student data into database

--- database/factories/StudentFactory.php
+++ database/factories/StudentFactory.php
@@ -18,7 +18,21 @@ class StudentFactory extends Factory
     {
         $faker = fake("en_IN");
         return [
-            
+            "roll_number" => $faker->numerify("########"),
+            "student_name" => $faker->name(),
+            "dob" => $faker->date("Y-m-d", "2007"),
+            "gender" => $faker->randomElement(["Male", "Female", "Other"]),
+            "student_email" => $faker->unique()->safeEmail(),
+            "student_contact" => $faker->unique()->numberBetween(9000000000, 9999999999),
+            "student_photo" => $faker->imageUrl(640, 480, "students", true),
+            "guardian_name" => $faker->name(),
+            "relation" => $faker->randomElement(["father", "grand_father", "mother", "grand_mother"]),
+            "guardian_email" => $faker->unique()->safeEmail(),
+            "guardian_contact" => $faker->unique()->numberBetween(9000000000, 9999999999),
+            "address" => $faker->address(),
+            "city" => $faker->city(),
+            "created_at" => now(),
+            "updated_at" => now()
         ];
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use Database\Factories\StudentFactory;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,9 +16,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        StudentFactory::new()->count(100)->create();
         // User::factory(10)->create();
-        $this->call([
-            AdminSeeder::class
-        ]);
+        // $this->call([
+        //     AdminSeeder::class
+        // ]);
     }
 }
